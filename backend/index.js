fcc28733ce7ef9e11690cf5de3478505fce80d8c
Wieczorovskyyy <xtubusek10123@gmail.com>
2026-05-09 06:59:20 +0200

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,34 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("../routes/auth");
+const appointmentRoutes = require("../routes/appointments");
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://carservicemanager-zojs-git-main-wieczorovskyyys-projects.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/appointments", require("./routes/appointments"));
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend działa"
+  });
 });
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "API działa poprawnie"
+  });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/appointments", appointmentRoutes);
+
+module.exports = app;
